9347309b809107bf3019eec814ba094b3b25fa8a
ReemKhalil18499 <khalil.reem18499@gmail.com>
2021-08-26 13:32:19 +0300

    new

--- app.js
+++ app.js
@@ -207,7 +207,7 @@ Student.prototype.scope = function() {
 Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scopeArrow());
+ console.log(joe.scopeArrow());
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
@@ -216,3 +216,4 @@ Student.prototype.scopeArrow = () => console.log(this);
 //
 // 3. Explain why "this" is different when an arrow function is used.
 //
+// --
